21b4c2366b945180d560055af5e976c2ad08bdfa
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-09-22 18:27:31 +0530

    printtitle and amount

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,7 +19,8 @@ class MyHomePage extends StatelessWidget {
     Transaction(id: '101', Title: 'food', amount: 89, date: DateTime.now()),
     Transaction(id: '102', Title: 'travel', amount: 65, date: DateTime.now()),
   ];
-
+  String? titleInput;
+  String? amtinput;
   // final MainAxisAlignment mainAxisAlignment,
   @override
   Widget build(BuildContext context) {
@@ -30,7 +32,8 @@ class MyHomePage extends StatelessWidget {
             // Ma
             // MainAxisAlignment _mainAxisAlignment=MainAxisAlignment.spaceAround,
             Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -40,16 +43,68 @@ class MyHomePage extends StatelessWidget {
                 child: Text('hello chart'),
               ),
             ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                        decoration: InputDecoration(labelText: 'text'),
+                        onChanged: (value) => titleInput = value),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                      onChanged: (value) => amtinput = value,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print(titleInput);
+                        print(amtinput);
+                      },
+                      child: Text(
+                        "Add Transaction",
+                        style: TextStyle(color: Colors.purple),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Column(
               children: transaction.map((e) {
                 return Card(
                   child: Row(
                     children: [
                       Container(
-                        child: Text(e.amount.toString()),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          // e.amount.toString(),
+                          '\$${e.amount}',
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                       Column(
-                        children: [Text(e.Title), Text(e.date.toString())],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            e.Title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            // e.date.toString(),
+                            DateFormat.yMMMd().format(e.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
                       )
                     ],
                   ),
