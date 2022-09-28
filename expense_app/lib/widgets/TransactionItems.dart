45c8fa17945be87bcb7f5866c0980b4da0cbd6f0
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-09-28 15:15:13 +0530

    change color using init State

--- expense_app/lib/widgets/TransactionItems.dart
+++ expense_app/lib/widgets/TransactionItems.dart
@@ -1,8 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TranasactionItems extends StatelessWidget {
+class TranasactionItems extends StatefulWidget {
   final Transaction transaction_;
   const TranasactionItems({
     Key? key,
@@ -13,30 +14,50 @@ class TranasactionItems extends StatelessWidget {
   final Function d_trans;
 
   @override
+  State<TranasactionItems> createState() => _TranasactionItemsState();
+}
+
+class _TranasactionItemsState extends State<TranasactionItems> {
+  Color? bgColor;
+  @override
+  void initState() {
+    const availablecolor = [
+      Colors.blue,
+      Colors.yellow,
+      Colors.pink,
+      Colors.orange
+    ];
+    bgColor = availablecolor[Random().nextInt(4)];
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
       child: ListTile(
           leading: CircleAvatar(
+            backgroundColor: bgColor,
             radius: 20,
             child: Padding(
               padding: EdgeInsets.all(10),
               child: FittedBox(
-                child: Text('\$${transaction_.amount}'),
+                child: Text('\$${widget.transaction_.amount}'),
               ),
             ),
           ),
           title: Text(
-            transaction_.Title,
+            widget.transaction_.Title,
             style: Theme.of(context).textTheme.headline6,
           ),
           subtitle: Text(
-            DateFormat.yMMMd().format(transaction_.date),
+            DateFormat.yMMMd().format(widget.transaction_.date),
           ),
           trailing: MediaQuery.of(context).size.width > 420
               ? TextButton.icon(
-                  onPressed: () => d_trans(transaction_.id),
+                  onPressed: () => widget.d_trans(widget.transaction_.id),
                   icon: const Icon(
                     Icons.delete,
                   ),
@@ -45,7 +66,7 @@ class TranasactionItems extends StatelessWidget {
                       foregroundColor: Color.fromARGB(255, 255, 0, 0)),
                 )
               : IconButton(
-                  onPressed: () => d_trans(transaction_.id),
+                  onPressed: () => widget.d_trans(widget.transaction_.id),
                   color: Color.fromARGB(255, 255, 0, 0),
                   icon: const Icon(
                     Icons.delete,
