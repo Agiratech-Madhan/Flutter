dbd0cbc844bcbaa69dcdbbae5a68db5ce02e55a6
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-09-29 19:05:32 +0530

    listview images

--- imageview/lib/main.dart
+++ imageview/lib/main.dart
@@ -8,7 +8,69 @@ class MyApp extends StatelessWidget {
   // const MyApp({super.key});
 
   // This widget is the root of your application.
+  List images_ = ["assets/images/sam.jpg", "assets/images/sam.jpg"];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: ListView.builder(
+          itemBuilder: (BuildContext ctx, index) {
+            return Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      child: Image.asset(images_[index]),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20, left: 20),
+                      // alignment: Alignment.center,
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("hello"),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            );
+          },
+          itemCount: images_.length,
+        ),
+      ),
+    );
+  }
+}
 
+
+
+
+
+
+
+
+
+
+
+
+
+/***import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({super.key});
+
+  // This widget is the root of your application.
+  List images_ = ["assets/images/sam.jpg", "assets/images/sam.jpg"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,10 +82,20 @@ class MyApp extends StatelessWidget {
           body: Column(
         children: [
           Container(
-            child: Image(image: AssetImage("assets/images/sam.jpg")),
+            child: Image(
+                image: AssetImage(
+              "assets/images/sam.jpg",
+            )),
           ),
+          ListView.builder(itemBuilder: (BuildContext ctx,index){
+              return Image.asset(images_[index]);
+
+          },
+          itemCount: images_.length,),
         ],
+        
       )),
     );
   }
 }
+ */
