c929b2946bb9a115b234e00ae2fb51b964e6dba8
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-10 19:03:48 +0530

    update status pending

---
+++ slack/lib/updatestatus.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class updatestatus extends StatefulWidget {
+  const updatestatus({super.key});
+
+  @override
+  State<updatestatus> createState() => _updatestatusState();
+}
+
+class _updatestatusState extends State<updatestatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
