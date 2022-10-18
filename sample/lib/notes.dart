886f9e847998ad3488d3be2630b91edfb6a96027
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-18 21:47:09 +0530

    Notes added

---
+++ sample/lib/notes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GridViewExample extends StatefulWidget {
+  const GridViewExample({super.key});
+
+  @override
+  State<GridViewExample> createState() => _GridViewExampleState();
+}
+
+class _GridViewExampleState extends State<GridViewExample> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+        children: [],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20));
+  }
+}
