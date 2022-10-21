16a140713fdbb022cc534e73a50aae49c0259d2e
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-21 09:31:53 +0530

    use colorscheme in muliple places

--- shopapp/lib/main.dart
+++ shopapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopapp/screens/products_overview_screens.dart';
 
 void main() {
@@ -16,8 +17,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        // primarySwatch: Colors.purple,
+        primaryColor: Colors.orange,
         colorScheme: ColorScheme.fromSeed(seedColor: primaryAccentColor),
+        textTheme: GoogleFonts.notoSansTextTheme(Theme.of(context).textTheme),
       ),
       home: ProductOverview(),
     );
--- shopapp/lib/widgets/product_item.dart
+++ shopapp/lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -13,19 +14,29 @@ class ProductItem extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imgUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
-        backgroundColor: Colors.black54,
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imgUrl,
+          fit: BoxFit.cover,
         ),
-        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        footer: GridTileBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).primaryColor,
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).primaryColor),
+        ),
       ),
     );
   }
