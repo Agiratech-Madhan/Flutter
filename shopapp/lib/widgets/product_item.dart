3d203f8138de16a9c17ff89c98731fb06cb28267
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-21 16:06:39 +0530

    use provider.of (with listen false)&use conusumer in favorite action only

--- shopapp/lib/widgets/product_item.dart
+++ shopapp/lib/widgets/product_item.dart
@@ -18,40 +18,41 @@ class ProductItem extends StatelessWidget {
   // }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<Product>(context);
-    return Consumer<Product>(
-      builder: (context, product, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                  arguments: product.id);
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
+    final product = Provider.of<Product>(context, listen: false);
+    print("rebuilds");
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
           ),
-          footer: GridTileBar(
-            leading: IconButton(
+        ),
+        footer: GridTileBar(
+          leading: Consumer<Product>(
+            builder: (context, product, child) => IconButton(
               onPressed: () {
                 product.toggleFavStatus();
               },
               icon: Icon(
                   product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              color: Theme.of(context).primaryColor,
+              color: Theme.of(context).colorScheme.secondary,
             ),
-            backgroundColor: Colors.black87,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.shopping_cart),
-                color: Theme.of(context).primaryColor),
           ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.shopping_cart),
+              color: Theme.of(context).primaryColor),
         ),
       ),
     );
