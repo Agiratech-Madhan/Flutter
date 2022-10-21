2615ab2c36dc31935015f6c3bffbf3ccae629192
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-21 15:59:07 +0530

    use consumer instead of(provider.of<>)

--- shopapp/lib/widgets/product_item.dart
+++ shopapp/lib/widgets/product_item.dart
@@ -18,38 +18,40 @@ class ProductItem extends StatelessWidget {
   // }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        footer: GridTileBar(
-          leading: IconButton(
-            onPressed: () {
-              product.toggleFavStatus();
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (context, product, child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
             },
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).primaryColor,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-          backgroundColor: Colors.black87,
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
+          footer: GridTileBar(
+            leading: IconButton(
+              onPressed: () {
+                product.toggleFavStatus();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).primaryColor,
+            ),
+            backgroundColor: Colors.black87,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.shopping_cart),
+                color: Theme.of(context).primaryColor),
           ),
-          trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.shopping_cart),
-              color: Theme.of(context).primaryColor),
         ),
       ),
     );
