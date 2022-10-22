efbc4a6d0e626cb8485eff68e0fab01a2ac3eb67
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-22 22:05:10 +0530

    add to cart details and function model

---
+++ shopapp/lib/providers/card.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CardItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+  CardItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Card with ChangeNotifier {
+  Map<String, CardItem>? _items;
+  Map<String, CardItem> get items {
+    return {..._items!};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items!.containsKey(productId)) {
+      _items!.update(
+        productId,
+        (existingCardItem) => CardItem(
+            id: existingCardItem.id,
+            title: existingCardItem.title,
+            quantity: existingCardItem.quantity + 1,
+            price: existingCardItem.price),
+      );
+    } else {
+      _items!.putIfAbsent(
+          productId,
+          (() => CardItem(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price)));
+    }
+  }
+}
