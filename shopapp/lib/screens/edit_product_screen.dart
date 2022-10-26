4aff73ed259a6c8e3d1c0cdf261c042e6b4569d0
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-26 13:02:02 +0530

    add image preview after done using focusnode,focusscope

--- shopapp/lib/screens/edit_product_screen.dart
+++ shopapp/lib/screens/edit_product_screen.dart
@@ -9,6 +9,17 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imgUrlController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imgUrlController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +43,51 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
             ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Description'),
+              maxLines: 3,
+              minLines: 1,
+              keyboardType: TextInputType.multiline,
+              // textInputAction: TextInputAction.next,
+              // onFieldSubmitted: (_) {
+              //   FocusScope.of(context).requestFocus(_priceFocusNode);
+              // },
+              focusNode: _descriptionFocusNode,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top: 8, right: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.grey),
+                  ),
+                  child: _imgUrlController.text.isEmpty
+                      ? Text('Enter a URL')
+                      : FittedBox(
+                          child: Image.network(
+                              (_imgUrlController.text).toString(),
+                              fit: BoxFit.cover),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.url,
+                    controller: _imgUrlController,
+                    decoration: InputDecoration(
+                      labelText: 'Img URL',
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         )),
       ),
