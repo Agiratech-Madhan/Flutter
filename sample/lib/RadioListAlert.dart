b28c71110da9e8053dec17a66bfb37866eaade88
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-28 18:58:52 +0530

    added ongenerate route and circular progress indicator

--- sample/lib/RadioListAlert.dart
+++ sample/lib/RadioListAlert.dart
@@ -1,101 +1,101 @@
-import 'package:flutter/material.dart';
-import 'package:slack/models/Notify.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter/material.dart';
+// // import 'package:slack/models/Notify.dart';
+// // import 'package:google_fonts/google_fonts.dart';
 
-class Notifyme extends StatefulWidget {
-  const Notifyme({super.key});
+// class Notifyme extends StatefulWidget {
+//   const Notifyme({super.key});
 
-  @override
-  State<Notifyme> createState() => _NotifymeState();
-}
+//   @override
+//   State<Notifyme> createState() => _NotifymeState();
+// }
 
-class _NotifymeState extends State<Notifyme> {
-  int notification_count = 0;
-  bool switchnotify = false;
-  String? _selection;
-  String result = '${notifyme[1].notify}';
-  String? mainresult;
-  void Changes(String value) {
-    setState(() {
-      // print("vdz");
-      result = value;
-    });
-  }
+// class _NotifymeState extends State<Notifyme> {
+//   int notification_count = 0;
+//   bool switchnotify = false;
+//   String? _selection;
+//   String result = '${notifyme[1].notify}';
+//   String? mainresult;
+//   void Changes(String value) {
+//     setState(() {
+//       // print("vdz");
+//       result = value;
+//     });
+//   }
 
-  Future<void> _dialogBuilder(BuildContext context) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Notify me on mobile...'),
-          content: StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: notifyme.length,
-                      itemBuilder: (context, index) => RadioListTile(
-                          title: Text(notifyme[index].notify),
-                          value: notifyme[index].notify,
-                          groupValue: result,
-                          onChanged: (value) {
-                            setState(() {
-                              Changes(value.toString());
-                            });
-                          }),
-                    ),
-                  )
-                ]);
-          }),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                    onPressed: () => {Navigator.pop(context)},
-                    child: Text('CANCEL')),
-                TextButton(
-                    onPressed: () {
-                      print('beforestate$mainresult');
-                      setState(() {
-                        mainresult = result;
-                      });
+//   Future<void> _dialogBuilder(BuildContext context) {
+//     return showDialog<void>(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: Text('Notify me on mobile...'),
+//           content: StatefulBuilder(
+//               builder: (BuildContext context, StateSetter setState) {
+//             return Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     height: MediaQuery.of(context).size.height * 0.6,
+//                     width: MediaQuery.of(context).size.width * 0.8,
+//                     child: ListView.builder(
+//                       shrinkWrap: true,
+//                       itemCount: notifyme.length,
+//                       itemBuilder: (context, index) => RadioListTile(
+//                           title: Text(notifyme[index].notify),
+//                           value: notifyme[index].notify,
+//                           groupValue: result,
+//                           onChanged: (value) {
+//                             setState(() {
+//                               Changes(value.toString());
+//                             });
+//                           }),
+//                     ),
+//                   )
+//                 ]);
+//           }),
+//           actions: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.end,
+//               children: [
+//                 TextButton(
+//                     onPressed: () => {Navigator.pop(context)},
+//                     child: Text('CANCEL')),
+//                 TextButton(
+//                     onPressed: () {
+//                       print('beforestate$mainresult');
+//                       setState(() {
+//                         mainresult = result;
+//                       });
 
-                      Navigator.pop(context);
-                      print(mainresult);
-                    },
-                    child: Text('SAVE'))
-              ],
-            ),
-          ],
-        );
-      },
-    );
-  }
+//                       Navigator.pop(context);
+//                       print(mainresult);
+//                     },
+//                     child: Text('SAVE'))
+//               ],
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListTile(
-        onTap: () => _dialogBuilder(context),
-        title: Text('Notify me on mobile', style: GoogleFonts.notoSans()),
-        subtitle: Text('$mainresult', style: GoogleFonts.notoSans()),
-      ),
-    );
-  }
-}
-/**onTap: () {
-                            print('object');
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ChatScreen(
-                                        c_icon: sizedBox,
-                                        c_name: chatdata[i].name,
-                                        DM: true)));
-                          }, */
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListTile(
+//         onTap: () => _dialogBuilder(context),
+//         // title: Text('Notify me on mobile', style: GoogleFonts.notoSans()),
+//         // subtitle: Text('$mainresult', style: GoogleFonts.notoSans()),
+//       ),
+//     );
+//   }
+// }
+// /**onTap: () {
+//                             print('object');
+//                             Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(
+//                                     builder: (context) => ChatScreen(
+//                                         c_icon: sizedBox,
+//                                         c_name: chatdata[i].name,
+//                                         DM: true)));
+//                           }, */
--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:language_picker/languages.dart';
 import 'package:language_picker/languages.g.dart';
 import 'package:sample/Message.dart';
+import 'package:sample/Ongenerate.dart';
 import 'package:sample/alertdialogue.dart';
 import 'package:sample/camera.dart';
 import 'package:sample/dates.dart';
@@ -55,157 +56,158 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Drawer1()));
-                    },
-                    child: Text("moveto drawer")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SnackBarDemo()));
-                    },
-                    child: Text("snackbar message")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ContainerExample()));
-                    },
-                    child: Text("container example ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Cards()));
-                    },
-                    child: Text("Card example ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Drop()));
-                    },
-                    child: Text("Expansiontile example ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MyStatefulWidget()));
-                    },
-                    child: Text("expansion example ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Radiosam()));
-                    },
-                    child: Text("Radio example ")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Emojies()));
-                    },
-                    child: Text("Emojies")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Dates()));
-                    },
-                    child: Text("dates")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Stacks()));
-                    },
-                    child: Text("Stack")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Sliver()));
-                    },
-                    child: Text("SLiverAppbar")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Searchbar()));
-                    },
-                    child: Text("SearchBar")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SearchList()));
-                    },
-                    child: Text("Searclist")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PageViewController()));
-                    },
-                    child: Text("Pageview")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Popupmenu()));
-                    },
-                    child: Text("popup")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => AlertDialogue()));
-                    },
-                    child: Text("AlertDialogue")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => CameraExample()));
-                    },
-                    child: Text("CameraExample")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Langauges()));
-                    },
-                    child: Text("Languages")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MicExample()));
-                    },
-                    child: Text("Mic")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Messages()));
-                    },
-                    child: Text("Messages")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Group1()));
-                    },
-                    child: Text("Groupedlistview")),
-              ],
-            ),
-          ),
-        ),
+        // body: SingleChildScrollView(
+        //   child: Container(
+        //     padding: EdgeInsets.all(10),
+        //     child: Column(
+        //       children: [
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Drawer1()));
+        //             },
+        //             child: Text("moveto drawer")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => SnackBarDemo()));
+        //             },
+        //             child: Text("snackbar message")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => ContainerExample()));
+        //             },
+        //             child: Text("container example ")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Cards()));
+        //             },
+        //             child: Text("Card example ")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Drop()));
+        //             },
+        //             child: Text("Expansiontile example ")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => MyStatefulWidget()));
+        //             },
+        //             child: Text("expansion example ")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Radiosam()));
+        //             },
+        //             child: Text("Radio example ")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Emojies()));
+        //             },
+        //             child: Text("Emojies")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Dates()));
+        //             },
+        //             child: Text("dates")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Stacks()));
+        //             },
+        //             child: Text("Stack")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Sliver()));
+        //             },
+        //             child: Text("SLiverAppbar")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Searchbar()));
+        //             },
+        //             child: Text("SearchBar")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => SearchList()));
+        //             },
+        //             child: Text("Searclist")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => PageViewController()));
+        //             },
+        //             child: Text("Pageview")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Popupmenu()));
+        //             },
+        //             child: Text("popup")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => AlertDialogue()));
+        //             },
+        //             child: Text("AlertDialogue")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => CameraExample()));
+        //             },
+        //             child: Text("CameraExample")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Langauges()));
+        //             },
+        //             child: Text("Languages")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(
+        //                   context,
+        //                   MaterialPageRoute(
+        //                       builder: (context) => MicExample()));
+        //             },
+        //             child: Text("Mic")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Messages()));
+        //             },
+        //             child: Text("Messages")),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               Navigator.push(context,
+        //                   MaterialPageRoute(builder: (context) => Group1()));
+        //             },
+        //             child: Text("Groupedlistview")),
+        //       ],
+        //     ),
+        //   ),
+        // ),
+        body: Generate(),
       ),
     );
   }
