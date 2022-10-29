8079c36f01977d44a6ef799a0f3d04135248b82c
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-10-29 16:57:23 +0530

    update the favorite status and error handling with firebase

--- shopapp/lib/providers/product.dart
+++ shopapp/lib/providers/product.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +17,25 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
-  void toggleFavStatus() {
+  void _setFav(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://shop-app-4b081-default-rtdb.firebaseio.com/products/$id.json');
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        _setFav(oldStatus);
+      }
+    } catch (e) {
+      _setFav(oldStatus);
+    }
   }
 }
