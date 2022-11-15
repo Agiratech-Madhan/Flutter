96576312998092c6a2f029a3428d9c129e0b0f9d
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-11-15 11:50:56 +0530

    initial commit

---
+++ RESTAPI/serializationexample/android/app/src/main/kotlin/com/example/serializationexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.serializationexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ RESTAPI/serializationexample/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:serializationexample/mainpage.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
+}
