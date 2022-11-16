d7d920c6ed597475660b8816a362d6a0b6f3d16b
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-11-16 13:18:57 +0530

    removed unwanted files

--- RESTAPI/apiwithfactory/lib/main.dart
+++ RESTAPI/apiwithfactory/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:apiwithfactory/pages/homepage.dart';
-import 'package:apiwithfactory/provider/photoprovider.dart';
-import 'package:apiwithfactory/widgets/editphoto.dart';
+import '../pages/homepage.dart';
+import '../provider/photoprovider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,8 +9,6 @@ void main() {
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-      routes: {EditPhoto.routeName: (context) => EditPhoto()},
-      // TodoPage()),
     ),
   ));
 }
