c1df3e92a0ddc5b6ff6c9ee7c5a48359b7373630
Agiratech-Maadhan <madhan.k@agiratech.com>
2022-12-02 10:51:52 +0530

    Slider, Range slider, draggable scrollable sheet

--- RESTAPI/counter/lib/pages/third_page.dart
+++ RESTAPI/counter/lib/pages/third_page.dart
@@ -12,6 +12,8 @@ class ThirdPage extends StatefulWidget {
 }
 
 class _ThirdPageState extends State<ThirdPage> {
+  double rating = 10;
+  RangeValues selectRange = const RangeValues(10, 20);
   @override
   Widget build(BuildContext context) {
     final value = Provider.of<DataProvider>(context);
@@ -25,9 +27,76 @@ class _ThirdPageState extends State<ThirdPage> {
               },
               icon: const Icon(Icons.arrow_forward))
         ],
-        title: Text('ThirdPage'),
+        title: const Text('ThirdPage'),
       ),
-      body: Center(child: Text('Counter Value  ${value.countValue}')),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            height: 70,
+            child: Card(
+              color: Colors.blue.withOpacity(0.3),
+              child: Center(
+                child: Text('Counter Value  ${value.countValue}'),
+              ),
+            ),
+          ),
+          Slider.adaptive(
+            value: rating,
+            onChanged: (x) {
+              setState(() {
+                rating = x;
+              });
+            },
+            activeColor: Colors.pink,
+            mouseCursor: MouseCursor.uncontrolled, //
+            thumbColor: Colors.green,
+            onChangeStart: (value) {
+              print('from $value to starting value $rating');
+            },
+            onChangeEnd: (value) {
+              print(value);
+            },
+            min: 1.0,
+            max: 100,
+            divisions: 10,
+            label: rating.toString(),
+          ),
+          RangeSlider(
+            values: selectRange,
+            max: 100,
+            divisions: 5,
+            inactiveColor: Colors.red,
+            labels: RangeLabels(
+              '${selectRange.start}',
+              '${selectRange.end}',
+            ),
+            onChanged: (RangeValues values) {
+              setState(() {
+                selectRange = values;
+              });
+            },
+          ),
+          Expanded(
+              child: DraggableScrollableSheet(
+                  expand: false,
+                  builder: ((context, scrollController) {
+                    return SingleChildScrollView(
+                      controller: scrollController,
+                      child: Column(
+                        children: [
+                          Image.network(
+                              'https://pbs.twimg.com/media/Eu7m692XIAEvxxP.png'),
+                          Container(
+                            color: Colors.red,
+                            height: 200,
+                          )
+                        ],
+                      ),
+                    );
+                  })))
+        ],
+      ),
     );
   }
 }
